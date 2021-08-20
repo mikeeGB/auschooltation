4f301ec5aa73ccb79ccd66c1adf4b4fbc5f7f7a7
mike <samkomisha@gmail.com>
2021-08-20 13:31:24 +0300

    rename home page

--- site_description/views.py
+++ site_description/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'site_description/home.html')
+    return render(request, 'site_description/home_landing.html')
