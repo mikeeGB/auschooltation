b61919aa18f75941e9d1851d7b45a1724ff180a4
mike <samkomisha@gmail.com>
2021-08-20 12:26:53 +0300

    add home view

--- site_description/views.py
+++ site_description/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Auschooltation Home page</h1>')
-
-
-def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'site_description/home.html')
