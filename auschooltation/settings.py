5efb43fec4fae2dfa1444017124e0c0ce79bbadf
mike <samkomisha@gmail.com>
2021-08-24 11:11:40 +0300

    start users application development

--- auschooltation/settings.py
+++ auschooltation/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'site_description.apps.SiteDescriptionConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
