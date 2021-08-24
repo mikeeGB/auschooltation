383a87d712af5ac0785abe4faadda6fb23779955
mike <samkomisha@gmail.com>
2021-08-24 14:34:24 +0300

    add handler of registration post method

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 # Create your views here.
 def register(request):
-    user_form = UserCreationForm()
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        user_form = UserRegisterForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            username = user_form.cleaned_data.get('username')
+            messages.success(request, f"Аккаунт создан для {username}!")
+            return redirect('auschool-home')
+    else:
+        user_form = UserRegisterForm(request.POST)
+    return render(request, 'users/register.html', {'user_form': user_form})
