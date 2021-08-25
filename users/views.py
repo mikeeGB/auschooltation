0bed62394c60fa9af04473dd993c7340270cc00d
mike <samkomisha@gmail.com>
2021-08-25 16:05:12 +0300

    add error checker dictionary

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
+    error_dict = {}
     if request.method == 'POST':
         user_form = UserRegisterForm(request.POST)
         if user_form.is_valid():
@@ -12,6 +13,8 @@ def register(request):
             username = user_form.cleaned_data.get('username')
             messages.success(request, f"Аккаунт создан для {username}!")
             return redirect('auschool-home')
+        else:
+            error_dict['errors'] = user_form.errors
     else:
         user_form = UserRegisterForm(request.POST)
-    return render(request, 'users/login_signup.html', {'user_form': user_form})
+    return render(request, 'users/login_signup.html', {'user_form': user_form, 'error_dict': error_dict})
