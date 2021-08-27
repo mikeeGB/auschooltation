87289a0db1861258ef606548d4b31b642cc1b22e
mike <samkomisha@gmail.com>
2021-08-27 15:12:42 +0300

    add authentication and login functions to log users in

--- users/views.py
+++ users/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from .forms import UserRegisterForm
 from .forms import UserLogInForm
 
 
 # Create your views here.
 def register_or_log_in(request):
+    """View for handling one of two post requests on the page of registration/log in"""
     error_dict = {}
+    # registration form
     if request.method == 'POST' and request.POST.get('submit') == 'reg':
         print(request.POST)
         user_reg_form = UserRegisterForm(request.POST)
         if user_reg_form.is_valid():
-            user_reg_form.save()
+            user_reg_form.save()  # save user to database
             username = user_reg_form.cleaned_data.get('username')
             messages.success(request, f"Аккаунт создан для {username}!")
-            return redirect('auschool-home')
+            return redirect('registration_and_login')
         else:
             error_dict['errors_reg'] = user_reg_form.errors
     else:
         user_reg_form = UserRegisterForm()
 
+    # log in form
     if request.method == 'POST' and request.POST.get('submit') == 'log_in':
         user_log_in_form = UserLogInForm(data=request.POST)
         if user_log_in_form.is_valid():
-            print(request.POST)
+            username = user_log_in_form.cleaned_data.get('username')
+            password = user_log_in_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('auschool-home')  # redirect to app page
         else:
             error_dict['log_in_errors'] = user_log_in_form.errors
     else:
