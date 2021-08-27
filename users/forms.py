846b6c6479844a913f4fde71e18be7e7fe9ab01d
mike <samkomisha@gmail.com>
2021-08-27 16:05:38 +0300

    change placeholder

--- users/forms.py
+++ users/forms.py
@@ -14,7 +14,7 @@ class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         """Update widget attributes to use custom css in rendered form fields"""
         super(UserRegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-style', 'placeholder': 'Your Full Name',
+        self.fields['username'].widget.attrs.update({'class': 'form-style', 'placeholder': 'Your Username',
                                                     'maxlength': '150', 'autocomplete': 'none'})
         self.fields['email'].widget.attrs.update({'class': 'form-style', 'placeholder': 'Your Email',
                                                   'autocomplete': 'none'})
