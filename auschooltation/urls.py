67c8f3e9fcac7d2aafd1357f7f473b73da18a96e
mike <samkomisha@gmail.com>
2021-08-27 16:49:11 +0300

    add url of profile page

--- auschooltation/urls.py
+++ auschooltation/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('site_description.urls')),
     path('register/', include('users.urls')),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.user_profile, name='profile')
 ]
