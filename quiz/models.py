072b39ffbbd0fa26cb21eeb749b1e24c3450298b
mike <samkomisha@gmail.com>
2021-09-05 13:08:38 +0300

    add models for questions, answers and sections

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Questions(models.Model):
+    section = models.ForeignKey('Sections', default='Section', on_delete=models.SET_DEFAULT)
+    question_text = models.TextField()
+    question_audio = models.FileField(upload_to='question_audio')
+
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+    def __str__(self):
+        return f"Question: {self.question_text}"
+
+
+class Answers(models.Model):
+    question = models.OneToOneField(Questions, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=60)
+
+    class Meta:
+        verbose_name = "Answer"
+        verbose_name_plural = "Answers"
+
+    def __str__(self):
+        return f"Answer: {self.answer_text}"
+
+
+class Sections(models.Model):
+    section_name = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name = "Section"
+        verbose_name_plural = "Sections"
+
+    def __str__(self):
+        return f"Section: {self.section_name}"
