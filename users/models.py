0dc457be0fb4200b5661327574e4b8bbb31c7a6d
mike <samkomisha@gmail.com>
2021-09-06 11:40:21 +0300

    add user statistics profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from quiz.models import UserAnswer
+
 
 # Create your models here.
+class UserProfileStat(models.Model):
+    user_answer = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
+    is_correct_answer = models.BooleanField()
+    answer_number = models.IntegerField()
