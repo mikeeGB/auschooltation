b8e91ffe7e227d367ed99c89034c4039cd690932
mike <samkomisha@gmail.com>
2021-09-08 12:53:03 +0300

    add two forms to quiz with radio button choices

---
+++ quiz/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .question_options import heart_tones_questions
+
+
+class QuestionFormHT1(forms.Form):
+    ht_q1_options = forms.CharField(label='Вопрос 1',
+                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_1_CHOICES))
+
+
+class QuestionFormHT2(forms.Form):
+    ht_q2_options = forms.CharField(label='Вопрос 2',
+                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_2_CHOICES))
