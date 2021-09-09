86156091aa4c71d4fb2e80448746d3032d5ef639
mike <samkomisha@gmail.com>
2021-09-09 15:47:20 +0300

    add third form

--- quiz/forms.py
+++ quiz/forms.py
@@ -10,3 +10,8 @@ class QuestionFormHT1(forms.Form):
 class QuestionFormHT2(forms.Form):
     ht_q2_options = forms.CharField(label='Вопрос 2',
                                     widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_2_CHOICES))
+
+
+class QuestionFormHT3(forms.Form):
+    ht_q3_options = forms.CharField(label='Вопрос 3',
+                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_3_CHOICES))
