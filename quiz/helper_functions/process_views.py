588d3538e6ddfe9970aa479997e2e013519363f0
mike <samkomisha@gmail.com>
2021-09-11 22:43:01 +0300

    add helper function for processing views

---
+++ quiz/helper_functions/process_views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from quiz.forms import QuestionForm
+from quiz.models import UserAnswer, Question
+
+
+def process_view(request, question_num: int, heart_tone_choice: list, redirect_page: str, render_page: str,
+                 form_name: str):
+    question = Question.objects.get(id=question_num)
+    if request.method == 'POST':
+        q_form = QuestionForm(heart_tone_choice, request.POST)
+        if q_form.is_valid():
+            current_user = request.user
+            answer = q_form.cleaned_data['options']
+            ua = UserAnswer(user=current_user, question=question, user_answer=answer)
+            ua.save()
+            return redirect(redirect_page)
+    else:
+        q_form = QuestionForm(heart_tone_choice)
+    return render(request, render_page, {form_name: q_form, 'question': question})
+
+
