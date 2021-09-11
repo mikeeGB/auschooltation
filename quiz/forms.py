89da4a0ee71f02ed2c6c28dd63a5cea9a449951c
mike <samkomisha@gmail.com>
2021-09-11 22:00:13 +0300

    refactor forms - use one form for every question

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,17 +1,8 @@
 from django import forms
-from .question_options import heart_tones_questions
 
 
-class QuestionFormHT1(forms.Form):
-    ht_q1_options = forms.CharField(label='Вопрос 1',
-                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_1_CHOICES))
+class QuestionForm(forms.Form):
 
-
-class QuestionFormHT2(forms.Form):
-    ht_q2_options = forms.CharField(label='Вопрос 2',
-                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_2_CHOICES))
-
-
-class QuestionFormHT3(forms.Form):
-    ht_q3_options = forms.CharField(label='Вопрос 3',
-                                    widget=forms.RadioSelect(choices=heart_tones_questions.HEART_TONE_3_CHOICES))
+    def __init__(self, options_list, *args, **kwargs):
+        super(QuestionForm, self).__init__(*args, **kwargs)
+        self.fields['options'] = forms.CharField(label='', widget=forms.RadioSelect(choices=options_list))
