b97337c3db38461d7c9a5355d598a567426883f5
mike <samkomisha@gmail.com>
2021-09-15 20:15:31 +0300

    add contact form to the view

--- site_description/views.py
+++ site_description/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from site_description.helper_functions.process_contact_form import process_form_tg
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'site_description/home_landing.html')
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            process_form_tg(contact_form)
+    else:
+        contact_form = ContactForm()
+    return render(request, 'site_description/home_landing.html', {'contact_form': contact_form})
