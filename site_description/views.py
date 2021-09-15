e762b244c439caaf734b62b066b3d42bda73b052
mike <samkomisha@gmail.com>
2021-09-15 20:41:59 +0300

    add success page for sent contact form

--- site_description/views.py
+++ site_description/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 from site_description.helper_functions.process_contact_form import process_form_tg
 
@@ -9,6 +9,11 @@ def home(request):
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             process_form_tg(contact_form)
+            return redirect('contact_form_success')
     else:
         contact_form = ContactForm()
     return render(request, 'site_description/home_landing.html', {'contact_form': contact_form})
+
+
+def contact(request):
+    return render(request, 'site_description/contact_form_success.html')
