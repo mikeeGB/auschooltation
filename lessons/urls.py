9be12a7994b65531fa29855bdefe5b6bf9d2c369
mike <samkomisha@gmail.com>
2021-09-19 21:21:22 +0300

    add heart_norm url

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,5 +3,5 @@ from .views import heart_basics, heart_norm
 
 urlpatterns = [
     path('heart_basics/', heart_basics, name='heart_basics'),
-    path('heart_norm/', heart_norm, name='heart_norm')
+    path('heart_norm/', heart_norm, name='heart_norm'),
 ]
